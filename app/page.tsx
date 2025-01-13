3701b34c3d0fdb0521b232e391aa399921951e04
Sebastian  Flores <sflores@cikume.com>
2025-01-13 10:28:11 -0600

    new branch

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             Clases de <span className='text-violet-950'>Matematicas</span>{' '}
             diferenciales
             <div className='bg-blue-500 w-full h-10 flex flex-1 items-center justify-center'>
-              <h3>solo sos paja maitro</h3>
+              <h3>solo sos paja maitro.</h3>
             </div>
           </h1>
         </>
